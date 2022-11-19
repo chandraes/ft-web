cce149347d5382063f36fbec1f04f1dc01db044f
UPT TIK Unsri <ka.tik@unsri.ac.id>
2022-11-19 14:26:58 +0700

    init

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.layout')
+@section('content')
+<div id="global-loader">
+    <img src="../assets/images/loader.svg" class="loader-img" alt="Loader">
+</div>
+<!-- /GLOABAL LOADER -->
+
+<!-- PAGE -->
+<div class="page login-page">
+    <div>
+        <!-- CONTAINER OPEN -->
+        <div class="col col-login mx-auto mt-7">
+            <div class="text-center">
+                <img src="{{asset('assets/images/brand/logo.png')}}" class="header-brand-img" alt="">
+            </div>
+        </div>
+        <div class="container-login100">
+            <div class="wrap-login100 p-0">
+                <div class="card-body">
+                        <form method="POST" action="{{ route('login') }}" class="login100-form validate-form">
+                            @csrf
+                        <span class="login100-form-title">
+                            {{ __('Login') }}
+                        </span>
+                        <div class="wrap-input100 validate-input" data-bs-validate = "Valid email is required: ex@abc.xyz">
+                            <input class="input100" type="text" name="email" placeholder="Email">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="zmdi zmdi-email" aria-hidden="true"></i>
+                            </span>
+                            @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <div class="wrap-input100 validate-input" data-bs-validate = "Password is required">
+                            <input class="input100" type="password" name="password" placeholder="Password">
+                            <span class="focus-input100"></span>
+                            <span class="symbol-input100">
+                                <i class="zmdi zmdi-lock" aria-hidden="true"></i>
+                            </span>
+                            @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                        </div>
+                        <div class="text-end pt-1">
+                            @if (Route::has('password.request'))
+                            <a class="text-primary ms-1" href="{{ route('password.request') }}">
+                                {{ __('Forgot Your Password?') }}
+                            </a>
+                        @endif
+                        </div>
+                        <div class="container-login100-form-btn">
+                            <button type="submit" class="login100-form-btn btn-primary">
+                                {{ __('Login') }}
+                            </button>
+                        </div>
+                        <div class="text-center pt-3">
+                            <p class="text-dark mb-0">Not a member?<a href="register.html" class="text-primary ms-1">Create an Account</a></p>
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-center my-3">
+                        <span>Copyright <i class="fa fa-copyright"> 2022 FT Universitas Sriwijaya </span></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
+    <link id="style" href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+
+    <!-- STYLE CSS -->
+    <link href="{{asset('assets/css/style.css')}}" rel="stylesheet" />
+    <link href="{{asset('assets/css/plugins.css')}}" rel="stylesheet" />
+
+    <!--- FONT-ICONS CSS -->
+    <link href="{{asset('assets/css/icons.css')}}" rel="stylesheet" />
+    @stack('css')
+</head>
+
+<body class="login-img">
+    <div>
+        @yield('content')
+    </div>
+
+    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+
+    <!-- BOOTSTRAP JS -->
+    <script src="{{asset('assets/plugins/bootstrap/js/popper.min.js')}}"></script>
+    <script src="{{asset('assets/plugins/bootstrap/js/bootstrap.min.js')}}"></script>
+
+    <!-- SPARKLINE JS -->
+    <script src="{{asset('assets/js/jquery.sparkline.min.js')}}"></script>
+
+    <!-- CHART-CIRCLE JS -->
+    <script src="{{asset('assets/js/circle-progress.min.js')}}"></script>
+
+    <!-- Perfect SCROLLBAR JS-->
+    <script src="{{asset('assets/plugins/p-scroll/perfect-scrollbar.js')}}"></script>
+
+    <!-- INPUT MASK JS -->
+    <script src="{{asset('assets/plugins/input-mask/jquery.mask.min.js')}}"></script>
+
+    <!-- Color Theme js -->
+    <script src="{{asset('assets/js/themeColors.js')}}"></script>
+
+    <!-- swither styles js -->
+    <script src="{{asset('assets/js/swither-styles.js')}}"></script>
+
+    <!-- CUSTOM JS -->
+    <script src="{{asset('assets/js/custom.js')}}"></script>
+    @stack('js')
+</body>
+
+</html>
