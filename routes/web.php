c09272b980e9d4fb5fc0afa2adb175fe42a0155c
katikunsri1960 <ka.tik@unsri.ac.id>
2022-11-24 07:44:20 +0000

    disable register

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
+Auth::routes([
+    'register' => false,
+]);
 
 Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 
